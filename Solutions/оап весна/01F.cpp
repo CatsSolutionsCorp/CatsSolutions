b0ecb3d55e0973e15893f4b8441f776202081960
SE04 <54131716+Meppapza@users.noreply.github.com>
2023-04-04 12:55:16 +1000

    Bucket sort (LETTERS ONLY)

---
+++ Solutions/оап весна/01F.cpp
@@ -0,0 +1,99 @@
+/**
+⠄⠄⠄⠄⠄⠄⠄⠄⣠⣴⠶⠿⠛⠛⠛⠛⠛⠛⠷⠶⢶⣤⣀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⣤⣶⠟⠉⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠈⠛⢿⣶⣤⠄⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⣠⣾⠟⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠻⣿⣿⣦⡀⠄⠄⠄⠄⠄
+⠄⠄⣼⣿⠋⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠹⣿⣿⣿⣆⠄⠄⠄⠄
+⠄⣸⣿⡇⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢸⣿⣿⣿⣧⡀⠄⠄
+⢰⣿⣿⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢰⣿⣿⣿⣿⣇⠄⠄
+⢸⣿⣿⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣿⣿⣿⣿⣿⣿⡀⠄
+⢸⣿⣿⢠⣤⣤⣄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣀⣀⣠⣤⡀⠄⢸⣿⣿⡟⣿⣿⡇⠄
+⢸⣿⣿⣿⠏⠛⠛⠿⣷⣶⣄⠄⠄⠄⠄⣠⣴⣾⣿⡿⠟⠛⠛⠛⠛⢦⣼⣿⣿⡔⣾⣿⡇⠄
+⠄⢿⣿⡟⢀⣤⣶⣶⠾⢿⣿⣷⠄⠄⢠⣿⣿⢛⡿⢿⣷⡶⠦⣤⣄⡈⢿⣿⣿⣿⣾⣿⠃⠄
+⣶⢾⣿⠄⠉⠁⠉⠉⠄⠤⠞⣿⠄⠄⢸⣿⡿⠄⠈⠄⠄⠁⠉⣽⠟⠄⠈⣿⣿⣿⣿⣿⣿⡇
+⣿⣿⣿⠄⠄⠄⠄⠄⠄⠄⠄⠃⠄⠄⢸⣿⡇⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣿⣿⣿⣿⣇⡏⣧
+⣿⣿⣿⡇⠄⠄⠄⠄⠄⡴⠄⠣⡀⢀⣸⣿⠿⣷⠄⠄⠄⠄⠄⠄⠄⠄⣠⣿⣿⣿⣿⣿⣿⢸
+⢹⣿⣿⣧⠄⠄⠄⠄⠄⠙⠚⣛⡊⣻⣿⡛⠗⠋⠄⠄⠄⠄⠄⠄⠄⢰⣿⣿⣿⣿⣿⣿⠏⡜
+⠈⢻⣿⣿⡆⠄⠄⢀⣴⣴⣿⣿⠿⢾⡻⠿⢿⣶⣶⣦⡀⠄⠄⠄⠄⣿⣿⣿⣿⣿⣿⣿⡜⠁
+⠄⠄⣿⣿⣿⠄⠄⣾⣿⣿⠿⠛⠛⠛⠛⠿⠶⣿⣿⣿⣿⡆⠄⠄⢀⣿⣿⣿⣿⣿⡿⠟⠄⠄
+⠄⠄⠄⢹⣿⣄⠄⠉⠁⠄⠳⢶⣶⣶⣶⣶⣾⣿⡟⠃⠄⠁⠄⠄⣼⣿⣿⣿⣿⣏⠄⠄⠄⠄
+⠄⠄⠄⠘⣿⣿⣆⣀⣀⡀⡀⡖⣸⣶⣶⣿⣿⣿⣷⣦⡀⠄⢀⣾⣿⣿⣿⣿⣿⣿⠄⠄⠄⠄
+⠄⠄⠄⠄⣿⣿⣿⣿⣿⠟⠁⠁⢿⣿⡿⠍⠻⣿⣿⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⠄⠄⠄⠄
+⠄⠄⠄⣼⣿⣿⡿⣿⣿⣴⠄⠰⣿⣿⣇⢀⠄⠸⣿⣿⣿⣿⣿⣿⠟⣿⣿⣿⣿⣿⡀⠄⠄⠄
+⠄⠄⠘⠿⣿⣿⣿⣀⠙⠿⣷⣤⣼⣿⣿⣾⣷⣾⣿⡿⠟⠋⠉⠄⠄⣿⣿⣿⣿⣿⠇⠄⠄⠄
+⠄⠄⠄⠄⠈⠛⠿⣿⣶⣤⡈⠛⢿⣿⣿⠿⠛⠉⠁⠄⠄⠄⠄⠄⠄⣿⣿⣿⣿⣿⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⠄⠄⠈⠛⠿⣿⣿⣦⣤⣀⡀⠄⠄⠄⠄⠄⠄⠄⠄⢘⣿⣿⣿⣿⡿⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠉⠉⠛⠻⠿⠿⢶⣦⣄⣀⣀⣀⣀⣼⡿⠿⠿⠛⠁⠄⠄⠄⠄
+*****************************************
+*        Copyrighted by SE04#0556       *
+*****************************************
+**/
+
+#include <fstream>
+#include <string>
+#include <iostream>
+
+int GetMax(const std::string *array, int size_of_array) {
+    int maximum = array[0].length() - 1;
+    for (int i = 1; i < size_of_array; i++)
+        if (array[i].length() - 1 > maximum)
+            maximum = array[i].length() - 1;
+    return maximum;
+}
+
+char GetMinSymbol(const std::string *array, int size_of_array) {
+    char minimum = array[0][0];
+    for (int i = 0; i < size_of_array; i++)
+        for (int k = 0; k < array[0].length(); k++)
+            if (array[i][k] < minimum)
+                minimum = array[i][k];
+    return minimum;
+}
+
+char GetMaxSymbol(const std::string *array, int size_of_array) {
+    char maximum = array[0][0];
+    for (int i = 0; i < size_of_array; i++)
+        for (int k = 0; k < array[0].length(); k++)
+            if (array[i][k] > maximum)
+                maximum = array[i][k];
+    return maximum;
+}
+
+void CountSort(std::string *array, int size_of_array, int exp, char min_symbol, char max_symbol) {
+    std::string output[size_of_array];
+    int count[max_symbol - min_symbol + 1];
+    for (int i = 0; i < max_symbol - min_symbol + 1; i++)
+        count[i] = 0;
+    for (int i = 0; i < size_of_array; i++)
+        count[array[i][exp] - min_symbol]++;
+    for (int i = 1; i < max_symbol - min_symbol + 1; i++)
+        count[i] += count[i - 1];
+    for (int i = size_of_array - 1; i >= 0; i--)
+        output[count[array[i][exp] - min_symbol]-- - 1] = array[i];
+    for (int i = 0; i < size_of_array; i++)
+        array[i] = output[i];
+}
+
+void RadixSort(std::string *array, int size_of_array) {
+    int max_length_of_string = GetMax(array, size_of_array);
+    char min_symbol = GetMinSymbol(array, size_of_array), max_symbol = GetMaxSymbol(array, size_of_array);
+    for (int exp = max_length_of_string; exp >= 0; exp--)
+        CountSort(array, size_of_array, exp, min_symbol, max_symbol);
+}
+
+int main() {
+    std::ifstream input("input.txt");
+    std::ofstream output("output.txt");
+    int size_of_array;
+    input >> size_of_array;
+    std::string array[size_of_array];
+    for (int i = 0; i < size_of_array; i++)
+        input >> array[i];
+    input.close();
+
+    RadixSort(array, size_of_array);
+
+    for (int i = 0; i < size_of_array; i++)
+        output << array[i] << '\n';
+    output.close();
+    return 0;
+}
