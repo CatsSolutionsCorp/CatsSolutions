47aa11e57833d081547f2650739c5b336e121fc1
SE04 <54131716+Meppapza@users.noreply.github.com>
2023-04-04 12:55:37 +1000

    Золотая середина

---
+++ Solutions/оап весна/02A.cpp
@@ -0,0 +1,123 @@
+/**
+⠄⠄⠄⠄⠄⠄⠄⠄⣠⣴⠶⠿⠛⠛⠛⠛⠛⠛⠷⠶⢶⣤⣀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⣤⣶⠟⠉⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠈⠛⢿⣶⣤⠄⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⣠⣾⠟⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠻⣿⣿⣦⡀⠄⠄⠄⠄⠄
+⠄⠄⣼⣿⠋⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠹⣿⣿⣿⣆⠄⠄⠄⠄
+⠄⣸⣿⡇⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢸⣿⣿⣿⣧⡀⠄⠄
+⢰⣿⣿⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢰⣿⣿⣿⣿⣇⠄⠄
+⢸⣿⣿⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣿⣿⣿⣿⣿⣿⡀⠄
+⢸⣿⣿⢠⣤⣤⣄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣀⣀⣠⣤⡀⠄⢸⣿⣿⡟⣿⣿⡇⠄
+⢸⣿⣿⣿⠏⠛⠛⠿⣷⣶⣄⠄⠄⠄⠄⣠⣴⣾⣿⡿⠟⠛⠛⠛⠛⢦⣼⣿⣿⡔⣾⣿⡇⠄
+⠄⢿⣿⡟⢀⣤⣶⣶⠾⢿⣿⣷⠄⠄⢠⣿⣿⢛⡿⢿⣷⡶⠦⣤⣄⡈⢿⣿⣿⣿⣾⣿⠃⠄
+⣶⢾⣿⠄⠉⠁⠉⠉⠄⠤⠞⣿⠄⠄⢸⣿⡿⠄⠈⠄⠄⠁⠉⣽⠟⠄⠈⣿⣿⣿⣿⣿⣿⡇
+⣿⣿⣿⠄⠄⠄⠄⠄⠄⠄⠄⠃⠄⠄⢸⣿⡇⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣿⣿⣿⣿⣇⡏⣧
+⣿⣿⣿⡇⠄⠄⠄⠄⠄⡴⠄⠣⡀⢀⣸⣿⠿⣷⠄⠄⠄⠄⠄⠄⠄⠄⣠⣿⣿⣿⣿⣿⣿⢸
+⢹⣿⣿⣧⠄⠄⠄⠄⠄⠙⠚⣛⡊⣻⣿⡛⠗⠋⠄⠄⠄⠄⠄⠄⠄⢰⣿⣿⣿⣿⣿⣿⠏⡜
+⠈⢻⣿⣿⡆⠄⠄⢀⣴⣴⣿⣿⠿⢾⡻⠿⢿⣶⣶⣦⡀⠄⠄⠄⠄⣿⣿⣿⣿⣿⣿⣿⡜⠁
+⠄⠄⣿⣿⣿⠄⠄⣾⣿⣿⠿⠛⠛⠛⠛⠿⠶⣿⣿⣿⣿⡆⠄⠄⢀⣿⣿⣿⣿⣿⡿⠟⠄⠄
+⠄⠄⠄⢹⣿⣄⠄⠉⠁⠄⠳⢶⣶⣶⣶⣶⣾⣿⡟⠃⠄⠁⠄⠄⣼⣿⣿⣿⣿⣏⠄⠄⠄⠄
+⠄⠄⠄⠘⣿⣿⣆⣀⣀⡀⡀⡖⣸⣶⣶⣿⣿⣿⣷⣦⡀⠄⢀⣾⣿⣿⣿⣿⣿⣿⠄⠄⠄⠄
+⠄⠄⠄⠄⣿⣿⣿⣿⣿⠟⠁⠁⢿⣿⡿⠍⠻⣿⣿⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⠄⠄⠄⠄
+⠄⠄⠄⣼⣿⣿⡿⣿⣿⣴⠄⠰⣿⣿⣇⢀⠄⠸⣿⣿⣿⣿⣿⣿⠟⣿⣿⣿⣿⣿⡀⠄⠄⠄
+⠄⠄⠘⠿⣿⣿⣿⣀⠙⠿⣷⣤⣼⣿⣿⣾⣷⣾⣿⡿⠟⠋⠉⠄⠄⣿⣿⣿⣿⣿⠇⠄⠄⠄
+⠄⠄⠄⠄⠈⠛⠿⣿⣶⣤⡈⠛⢿⣿⣿⠿⠛⠉⠁⠄⠄⠄⠄⠄⠄⣿⣿⣿⣿⣿⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⠄⠄⠈⠛⠿⣿⣿⣦⣤⣀⡀⠄⠄⠄⠄⠄⠄⠄⠄⢘⣿⣿⣿⣿⡿⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠉⠉⠛⠻⠿⠿⢶⣦⣄⣀⣀⣀⣀⣼⡿⠿⠿⠛⠁⠄⠄⠄⠄
+*****************************************
+*        Copyrighted by SE04#0556       *
+*****************************************
+**/
+
+#include <fstream>
+#include <string>
+#include <iostream>
+
+void Make_It_Max_Heap_Deletion(long long *array, long long size_of_heap, long long current_node) {
+    long long largest = current_node, left_child = 2 * current_node + 1, right_child = 2 * current_node + 2;
+    if (left_child < size_of_heap and array[left_child] > array[largest])
+        largest = left_child;
+    if (right_child < size_of_heap and array[right_child] > array[largest])
+        largest = right_child;
+    if (largest != current_node) {
+        std::swap(array[current_node], array[largest]);
+        Make_It_Max_Heap_Deletion(array, size_of_heap, largest);
+    }
+}
+
+void Delete_Node_Max_Heap(long long *array, long long &size_of_heap) {
+    array[0] = array[size_of_heap-- - 1];
+    Make_It_Max_Heap_Deletion(array, size_of_heap, 0);
+}
+
+void Make_It_Max_Heap_Insertion(long long *array, long long size_of_heap, long long current_node) {
+    long long parent = (current_node - 1) / 2;
+    if (parent >= 0 and array[current_node] > array[parent]) {
+            std::swap(array[current_node], array[parent]);
+            Make_It_Max_Heap_Insertion(array, size_of_heap, parent);
+        }
+}
+
+void Insert_Node_Max_Heap(long long *array, long long &size_of_heap, long long value) {
+    array[size_of_heap++] = value;
+    Make_It_Max_Heap_Insertion(array, size_of_heap, size_of_heap - 1);
+}
+
+void Make_It_Min_Heap_Deletion(long long *array, long long size_of_heap, long long current_node) {
+    long long smallest = current_node, left_child = 2 * current_node + 1, right_child = 2 * current_node + 2;
+    if (right_child < size_of_heap and array[right_child] < array[smallest])
+        smallest = right_child;
+    if (left_child < size_of_heap and array[left_child] < array[smallest])
+        smallest = left_child;
+    if (smallest != current_node) {
+        std::swap(array[current_node], array[smallest]);
+        Make_It_Min_Heap_Deletion(array, size_of_heap, smallest);
+    }
+}
+
+void Delete_Node_Min_Heap(long long *array, long long &size_of_heap) {
+    array[0] = array[size_of_heap-- - 1];
+    Make_It_Min_Heap_Deletion(array, size_of_heap, 0);
+}
+
+void Make_It_Min_Heap_Insertion(long long *array, long long size_of_heap, long long current_node) {
+    long long parent = (current_node - 1) / 2;
+    if (parent >= 0 and array[current_node] < array[parent]) {
+            std::swap(array[current_node], array[parent]);
+            Make_It_Min_Heap_Insertion(array, size_of_heap, parent);
+        }
+}
+
+void Insert_Node_Min_Heap(long long *array, long long &size_of_heap, long long value) {
+    array[size_of_heap++] = value;
+    Make_It_Min_Heap_Insertion(array, size_of_heap, size_of_heap - 1);
+}
+
+int main() {
+    std::ifstream input("input.txt");
+    std::ofstream output("output.txt");
+    int size_of_array;
+    input >> size_of_array;
+    long long max_heap[size_of_array], min_heap[size_of_array], size_of_max_heap = 0, size_of_min_heap = 0;
+
+    for (int i = 0; i < size_of_array; i++) {
+        long long value;
+        input >> value;
+        if (size_of_min_heap != 0 and value > min_heap[0])
+            Insert_Node_Min_Heap(min_heap, size_of_min_heap, value);
+        else
+            Insert_Node_Max_Heap(max_heap, size_of_max_heap, value);
+        while (size_of_min_heap > size_of_max_heap) {
+            Insert_Node_Max_Heap(max_heap, size_of_max_heap, min_heap[0]);
+            Delete_Node_Min_Heap(min_heap, size_of_min_heap);
+        }
+        while (size_of_min_heap + 1 < size_of_max_heap) {
+            Insert_Node_Min_Heap(min_heap, size_of_min_heap, max_heap[0]);
+            Delete_Node_Max_Heap(max_heap, size_of_max_heap);
+        }
+        output << max_heap[0] << ' ';
+    }
+
+    input.close();
+    output.close();
+    return 0;
+}
